0e681cbbcc73c81e35466986cb04174927ae29f0
Saad bajwa <saadbajwa659@gmail.com>
2024-02-20 17:33:02 +0500

    4. Action Creators,Redux and Thunk

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // REDUX SETUP
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware, compose } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
 import { Provider } from 'react-redux';
+import {thunk} from 'redux-thunk';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // Pass redux-thunk along with other default middleware
   devTools: process.env.NODE_ENV !== 'production',
 });
 
