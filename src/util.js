302122c6f33d2cdafa10f48a5a562e9773768644
Saad bajwa <saadbajwa659@gmail.com>
2024-03-09 14:43:39 +0500

    fixed imagePath error

--- src/util.js
+++ src/util.js
@@ -1,6 +1,7 @@
 // Media resize
 
 export const smallImage = (imagePath, size) => {
+    if (!imagePath || !size) return;
     const image = imagePath.match(/media\/screenshots/)
         ? imagePath.replace(
             "media/screenshots",
